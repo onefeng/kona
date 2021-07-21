1a507d7ef014260efdd011022724e57c840e8652
onefeng <15198086902@163.com>
2021-07-21 21:21:20 +0800

    添加数据库

---
+++ kona/data_source/sqlite_client.py
@@ -0,0 +1,72 @@
+import sqlite3
+import logging
+
+import pandas as pd
+from DBUtils.PersistentDB import PersistentDB
+
+
+class SqliteClient(object):
+    def __init__(self, conf):
+        db_path = conf.SQLITE_URL
+        self._pool = PersistentDB(sqlite3, maxusage=None, threadlocal=None, closeable=False, database=db_path)
+
+    def get_conn(self):
+        return self._pool.connection()
+
+    def query(self, sql):
+        results = ()
+        try:
+            db = self.get_conn()
+            cursor = db.cursor()
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            db.commit()
+            cursor.close()
+            db.close()
+        except Exception as e:
+            logging.exception(e)
+        finally:
+            return results
+
+    def noquery(self, sql):
+        flag = False
+        try:
+            db = self.get_conn()
+            cursor = db.cursor()
+            cursor.execute(sql)
+            db.commit()
+            cursor.close()
+            db.close()
+            flag = True
+        except Exception as e:
+            logging.exception(e)
+        finally:
+            return flag
+
+
+class SqliteFrame(object):
+    """pandas handler sqlite database"""
+
+    def __init__(self, conf):
+        db = conf.SQLITE_URL
+        self.con = sqlite3.connect(db)
+
+    def get_conn(self):
+        return self.con
+
+    def sqlite_to_df(self, sql):
+        """read sqlite to DataFrame"""
+        con = self.get_conn()
+        cursor = con.cursor()
+        values = cursor.execute(sql)
+        df = pd.DataFrame(data=values)
+        cursor.close()
+        con.close()
+        return df
+
+    def df_to_sqlite(self, name, df):
+        """write DataFrame to sqlite"""
+        con = self.get_conn()
+        df.to_sql(name, con, if_exists='replace')
+        con.close()
+        return name
